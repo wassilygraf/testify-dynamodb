ae2230e799ad44d62d7fdbb2d9e476fb4523281b
Wassily <wassilygraf@googlemail.com>
2020-02-24 21:12:43 +0100

    added bin folder and modified package to allow global installation

---
+++ bin/index.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+const { main } = require('../src/main');
+
+const DEFAULT_FOLDER = `${__dirname}/../stacks`;
+const [stackFolder] = process.argv.slice(2);
+
+main(stackFolder || DEFAULT_FOLDER);
